cffc20769c61c1fafabdab7db35b511e0a892f8e
Damien Pobel <dp@ez.no>
2014-12-04 10:56:12 +0100

    EZP-23128: Initialized the PlatformUIAssetsBundle

---
+++ EzSystemsPlatformUIAssetsBundle.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace EzSystems\PlatformUIAssetsBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class EzSystemsPlatformUIAssetsBundle extends Bundle
+{
+    protected $name = 'eZPlatformUIAssetsBundle';
+}
