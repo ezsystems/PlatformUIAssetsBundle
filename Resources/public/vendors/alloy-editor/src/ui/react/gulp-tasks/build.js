cb3e4dd242aa26d17082338f5251f9fd0ab5764f
Damien Pobel <dp@ez.no>
2015-06-23 08:25:35 +0200

    Version 0.7.0

---
+++ Resources/public/vendors/alloy-editor/src/ui/react/gulp-tasks/build.js
@@ -0,0 +1,234 @@
+'use strict';
+
+var gulp = require('gulp');
+
+var argv = require('yargs').argv;
+var react = require('gulp-react');
+var concat = require('gulp-concat');
+var del = require('del');
+var fs = require('fs');
+var path = require('path');
+var rename = require('gulp-rename');
+var replace = require('gulp-replace');
+var runSequence = require('run-sequence');
+var template = require('gulp-template');
+var uglify = require('gulp-uglify');
+var yuidoc = require('gulp-yuidoc-relative');
+var zip = require('gulp-zip');
+
+var rootDir = path.join(__dirname, '..', '..', '..', '..');
+var reactDir = path.join(rootDir, 'src', 'ui', 'react');
+var pkg = require(path.join(rootDir, 'package.json'));
+
+var apiFolder = path.join(rootDir, 'api');
+var distFolder = path.join(rootDir, 'dist');
+var editorDistFolder = path.join(distFolder, 'alloy-editor');
+
+var srcFiles = require('../_src.js');
+
+function errorHandler(error) {
+  console.log(error.toString());
+
+  this.emit('end');
+}
+
+gulp.task('build', function(callback) {
+    runSequence(
+        'clean-dist',
+        [
+            'build-css',
+            'build-js',
+            'copy-languages'
+        ],
+        [
+            'create-alloy-editor-all',
+            'create-alloy-editor-no-ckeditor',
+            'create-alloy-editor-no-react'
+        ],
+        'build-demo',
+        callback
+    );
+});
+
+gulp.task('release', function(callback) {
+    runSequence(
+        'clean-api',
+        'clean-dist',
+        [
+            'build-api',
+            'build-css',
+            'build-js',
+            'copy-languages'
+        ],
+        [
+            'minimize-css',
+            'minimize-alloy-editor-core'
+        ],
+        [
+            'create-alloy-editor-all',
+            'create-alloy-editor-all-min',
+            'create-alloy-editor-no-ckeditor',
+            'create-alloy-editor-no-ckeditor-min',
+            'create-alloy-editor-no-react',
+            'create-alloy-editor-no-react-min'
+        ],
+        'build-demo',
+        callback
+    );
+});
+
+gulp.task('build-api', function() {
+    var parseOpts = {
+        project: {
+            name: pkg.name,
+            description: pkg.description,
+            version: pkg.version,
+            url: pkg.homepage
+        }
+    };
+
+    var renderOpts = {
+        themedir: path.join(rootDir, 'api-theme')
+    };
+
+    gulp.src([
+        path.join(rootDir, 'src/core/**/*.js'),
+        path.join(rootDir, 'src/plugins/autolink.js'),
+        path.join(rootDir, 'src/plugins/drop-images.js'),
+        path.join(rootDir, 'src/plugins/placeholder.js'),
+        path.join(reactDir, 'src/**/*.js*')
+    ])
+    .pipe(yuidoc(parseOpts, renderOpts))
+    .pipe(gulp.dest(apiFolder));
+});
+
+gulp.task('build-demo', function() {
+    var templateHead;
+
+    if (argv._.indexOf('release') >= 0) {
+        templateHead = 'head-release.template';
+    }
+    else {
+        templateHead = 'head-dev.template';
+    }
+
+    return gulp.src([
+            path.join(reactDir, 'demo', 'index.html'),
+            path.join(reactDir, 'demo', 'bootstrap.css')
+        ])
+        .pipe(template({
+            resources: fs.readFileSync(path.join(reactDir, 'template', templateHead))
+        }))
+        .pipe(gulp.dest(path.join(distFolder)));
+});
+
+gulp.task('build-js', function(callback) {
+    runSequence([
+        'copy-ckeditor',
+        'create-alloy-editor-core'
+    ], 'wrap-alloy-editor', callback);
+});
+
+gulp.task('clean-api', function(callback) {
+    return del(apiFolder, callback);
+});
+
+gulp.task('clean-dist', function(callback) {
+    return del(distFolder, callback);
+});
+
+gulp.task('copy-ckeditor', function() {
+    return gulp.src(path.join(rootDir, 'lib', 'ckeditor', '/**'))
+        .pipe(gulp.dest(editorDistFolder));
+});
+
+gulp.task('copy-languages', function() {
+    return gulp.src(path.join(reactDir, 'lang', '/**'))
+        .pipe(gulp.dest(path.join(editorDistFolder, 'lang', 'alloy-editor')));
+});
+
+gulp.task('create-alloy-editor-all', function() {
+    return gulp.src([
+            path.join(editorDistFolder, 'ckeditor.js'),
+            path.join(reactDir, 'vendor', 'react.js'),
+            path.join(editorDistFolder, 'alloy-editor-core.js')
+        ])
+        .pipe(concat('alloy-editor-all.js'))
+        .pipe(replace(/ckeditor(\\?).js/g, 'alloy-editor-all$1.js'))
+        .pipe(gulp.dest(editorDistFolder));
+});
+
+gulp.task('create-alloy-editor-no-ckeditor', function() {
+    return gulp.src([
+            path.join(reactDir, 'vendor', 'react.js'),
+            path.join(editorDistFolder, 'alloy-editor-core.js')
+        ])
+        .pipe(concat('alloy-editor-no-ckeditor.js'))
+        .pipe(gulp.dest(editorDistFolder));
+});
+
+gulp.task('create-alloy-editor-no-react', function() {
+    return gulp.src([
+            path.join(editorDistFolder, 'ckeditor.js'),
+            path.join(editorDistFolder, 'alloy-editor-core.js')
+        ])
+        .pipe(concat('alloy-editor-no-react.js'))
+        .pipe(replace(/ckeditor(\\?).js/g, 'alloy-editor-no-react$1.js'))
+        .pipe(gulp.dest(editorDistFolder));
+});
+
+gulp.task('create-alloy-editor-all-min', function() {
+    return gulp.src([
+            path.join(editorDistFolder, 'ckeditor.js'),
+            path.join(reactDir, 'vendor', 'react-min.js'),
+            path.join(editorDistFolder, 'alloy-editor-core-min.js')
+        ])
+        .pipe(concat('alloy-editor-all-min.js'))
+        .pipe(replace(/ckeditor(\\?).js/g, 'alloy-editor-all-min$1.js'))
+        .pipe(gulp.dest(editorDistFolder));
+});
+
+gulp.task('create-alloy-editor-no-ckeditor-min', function() {
+    return gulp.src([
+            path.join(reactDir, 'vendor', 'react-min.js'),
+            path.join(editorDistFolder, 'alloy-editor-core-min.js')
+        ])
+        .pipe(concat('alloy-editor-no-ckeditor-min.js'))
+        .pipe(gulp.dest(editorDistFolder));
+});
+
+gulp.task('create-alloy-editor-no-react-min', function() {
+    return gulp.src([
+            path.join(editorDistFolder, 'ckeditor.js'),
+            path.join(editorDistFolder, 'alloy-editor-core-min.js')
+        ])
+        .pipe(concat('alloy-editor-no-react-min.js'))
+        .pipe(replace(/ckeditor(\\?).js/g, 'alloy-editor-no-react-min$1.js'))
+        .pipe(gulp.dest(editorDistFolder));
+});
+
+gulp.task('create-alloy-editor-core', function() {
+    return gulp.src(srcFiles, {cwd : rootDir + '/src'})
+    .pipe(react()).on('error', errorHandler)
+    .pipe(concat('alloy-editor-core.js'))
+    .pipe(gulp.dest(editorDistFolder));
+});
+
+gulp.task('release:zip', ['release'], function() {
+    return gulp.src(path.join(distFolder, '/**'))
+        .pipe(zip('alloy-editor-' + pkg.version + '.zip'))
+        .pipe(gulp.dest(distFolder));
+});
+
+gulp.task('minimize-alloy-editor-core', function() {
+    return gulp.src([
+            path.join(editorDistFolder, 'alloy-editor-core.js')
+        ])
+        .pipe(uglify())
+        .pipe(rename('alloy-editor-core-min.js'))
+        .pipe(gulp.dest(editorDistFolder));
+});
+
+gulp.task('watch', ['build'], function () {
+    gulp.watch('src/**/*', ['build']);
+});
---
+++ Resources/public/vendors/alloy-editor/src/ui/react/src/components/toolbars/toolbar-add.jsx
@@ -0,0 +1,218 @@
+(function () {
+    'use strict';
+
+    /**
+     * The ToolbarAdd class provides functionality for adding content to the editor.
+     *
+     * @uses WidgetDropdown
+     * @uses WidgetExclusive
+     * @uses WidgetFocusManager
+     * @uses ToolbarButtons
+     * @uses WidgetPosition
+     * @uses WidgetArrowBox
+     *
+     * @class ToolbarAdd
+     */
+    var ToolbarAdd = React.createClass({
+        mixins: [AlloyEditor.WidgetDropdown, AlloyEditor.WidgetExclusive, AlloyEditor.WidgetFocusManager, AlloyEditor.ToolbarButtons, AlloyEditor.WidgetPosition, AlloyEditor.WidgetArrowBox],
+
+        // Allows validating props being passed to the component.
+        propTypes: {
+            /**
+             * The toolbar configuration.
+             *
+             * @property {Object} config
+             */
+            config: React.PropTypes.object,
+
+            /**
+             * The editor instance where the component is being used.
+             *
+             * @property {Object} editor
+             */
+            editor: React.PropTypes.object.isRequired,
+
+            /**
+             * The gutter to be applied to the widget when rendered in exclusive mode
+             *
+             * @property {Object} gutterExclusive
+             */
+            gutterExclusive: React.PropTypes.object,
+
+            /**
+             * The label that should be used for accessibility purposes.
+             *
+             * @property {String} label
+             */
+            label: React.PropTypes.string,
+
+            /**
+             * The data, returned from {{#crossLink "CKEDITOR.plugins.selectionregion/getSelectionData:method"}}{{/crossLink}}
+             *
+             * @property {Object} selectionData
+             */
+            selectionData: React.PropTypes.object
+        },
+
+        // Lifecycle. Provides static properties to the widget.
+        statics: {
+            /**
+             * The name which will be used as an alias of the button in the configuration.
+             *
+             * @static
+             * @property {String} key
+             * @default add
+             */
+            key: 'add'
+        },
+
+        /**
+         * Lifecycle. Returns the default values of the properties used in the widget.
+         *
+         * @method getDefaultProps
+         * @return {Object} The default properties.
+         */
+        getDefaultProps: function() {
+            return {
+                circular: true,
+                descendants: '.alloy-editor-button',
+                gutterExclusive: {
+                    left: 10,
+                    top: 0
+                },
+                keys: {
+                    dismiss: [27],
+                    next: [39, 40],
+                    prev: [37, 38]
+                }
+            };
+        },
+
+        /**
+         * Lifecycle. Invoked once, only on the client (not on the server),
+         * immediately after the initial rendering occurs.
+         *
+         * @method componentDidMount
+         */
+        componentDidMount: function () {
+            this._updatePosition();
+        },
+
+        /**
+         * Lifecycle. Invoked immediately after the component's updates are flushed to the DOM.
+         * This method is not called for the initial render.
+         *
+         * @method componentDidUpdate
+         * @param {Object} prevProps The previous state of the component's properties.
+         * @param {Object} prevState Component's previous state.
+         */
+        componentDidUpdate: function (prevProps, prevState) {
+            this._updatePosition();
+
+            // In case of exclusive rendering, focus the first descendant (button)
+            // so the user will be able to start interacting with the buttons immediately.
+            if (this.props.renderExclusive) {
+                this.focus();
+            }
+        },
+
+        /**
+         * Lifecycle. Renders the buttons for adding content.
+         *
+         * @method render
+         * @return {Object} The content which should be rendered.
+         */
+        render: function() {
+            var buttons = this._getButtons();
+            var className = this._getToolbarClassName();
+
+            return (
+                <div aria-label={AlloyEditor.Strings.add} className={className} data-tabindex={this.props.config.tabIndex || 0} onFocus={this.focus} onKeyDown={this.handleKey} role="toolbar" tabIndex="-1">
+                    <div className="alloy-editor-container">
+                        {buttons}
+                    </div>
+                </div>
+            );
+        },
+
+        /**
+         * Returns a list of buttons that will eventually render to HTML.
+         *
+         * @protected
+         * @method _getButtons
+         * @return {Object} The buttons which have to be rendered.
+         */
+        _getButtons: function() {
+            var buttons;
+
+            if (this.props.renderExclusive) {
+                buttons = this.getToolbarButtons(this.props.config.buttons);
+            } else {
+                if (this.props.selectionData && this.props.selectionData.region) {
+                    buttons = (
+                        <button aria-label={AlloyEditor.Strings.add} className="alloy-editor-button alloy-editor-button-add" onClick={this.props.requestExclusive.bind(this, ToolbarAdd.key)} title={AlloyEditor.Strings.add}>
+                            <span className="alloy-editor-icon-add"></span>
+                        </button>
+                    );
+                }
+            }
+
+            return buttons;
+        },
+
+        /**
+         * Returns the class name of the toolbar in case of both exclusive and normal mode.
+         *
+         * @protected
+         * @method _getToolbarClassName
+         * @return {String} The class name which have to be applied to the DOM element.
+         */
+        _getToolbarClassName: function() {
+            var cssClass = 'alloy-editor-toolbar-add';
+
+            if (this.props.renderExclusive) {
+                cssClass = 'alloy-editor-toolbar ' + this.getArrowBoxClasses();
+            }
+
+            return cssClass;
+        },
+
+        /**
+         * Calculates and sets the position of the toolbar in exclusive or normal mode.
+         *
+         * @protected
+         * @method _updatePosition
+         */
+        _updatePosition: function() {
+            var region;
+
+            if (this.props.renderExclusive) {
+                this.updatePosition();
+                this.show();
+
+            } else {
+                if (this.props.selectionData) {
+                    region = this.props.selectionData.region;
+                }
+
+                if (region) {
+                    var domNode = React.findDOMNode(this);
+                    var domElement = new CKEDITOR.dom.element(domNode);
+
+                    var startRect = region.startRect || region;
+                    var left = this.props.editor.get('nativeEditor').editable().getClientRect().left;
+
+                    domNode.style.left = left - domNode.offsetWidth - this.props.gutterExclusive.left + 'px';
+                    domNode.style.top = region.top - domNode.offsetHeight/2 + startRect.height/2 + 'px';
+                    domNode.style.opacity = 1;
+
+                    domElement.removeClass('alloy-editor-arrow-box');
+
+                    this.cancelAnimation();
+                }
+            }
+        }
+    });
+
+    AlloyEditor.Toolbars[ToolbarAdd.key] = AlloyEditor.ToolbarAdd = ToolbarAdd;
+}());
