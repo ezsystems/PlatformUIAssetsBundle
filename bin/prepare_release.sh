e6caff499d96eda415a74004e067d005c88e4468
André R <andre.romcke@gmail.com>
2016-04-21 13:30:30 +0200

    Reduce size of asset bundle further
    YUI:
    - $YUI3_DIR/build/*/*-debug.js
    - $YUI3_DIR/build/*/*-coverage.js
    
    Alloy:
    - $ALLOY_DIR/api
    - $ALLOY_DIR/lib

--- bin/prepare_release.sh
+++ bin/prepare_release.sh
@@ -43,6 +43,8 @@ check_command "bower"
 VENDOR_DIR=`cat .bowerrc | grep "directory" | cut -d ':' -f 2 | sed 's/[ "]//g'`
 YUI3_DIR="$VENDOR_DIR/yui3"
 YUI3_NOTICE="$YUI3_DIR/YUI3_IN_PLATFORMUIASSETSBUNDLE.txt"
+ALLOY_DIR="$VENDOR_DIR/alloy-editor"
+ALLOY_NOTICE="$ALLOY_DIR/ALLOY_IN_PLATFORMUIASSETSBUNDLE.txt"
 
 CURRENT_BRANCH=`git branch | grep '*' | cut -d ' ' -f 2`
 TMP_BRANCH="version_$VERSION"
@@ -62,11 +64,18 @@ bower install
 check_process "run bower"
 
 echo "# Removing, docs, API docs and tests from YUI"
-rm -rf "$YUI3_DIR/api" "$YUI3_DIR/docs" "$YUI3_DIR/tests"
+rm -rf "$YUI3_DIR/api" "$YUI3_DIR/docs" "$YUI3_DIR/tests" "$YUI3_DIR/build/*/*-coverage.js" "$YUI3_DIR/build/*/*-debug.js"
 check_process "clean YUI"
 echo "This is a customized YUI3 version." > $YUI3_NOTICE
 echo "To decrease the size of the bundle, it does not include the API docs," >> $YUI3_NOTICE
-echo "the documentation and the unit tests" >> $YUI3_NOTICE
+echo "the documentation, the unit tests or the generated debug/coverage versions of YUI code." >> $YUI3_NOTICE
+
+echo "# Removing API docs and lib from alloy-editor"
+rm -rf "$ALLOY_DIR/api" "$ALLOY_DIR/lib"
+check_process "clean alloy-editor"
+echo "This is a customized Alloy version." > $ALLOY_NOTICE
+echo "To decrease the size of the bundle, it does not include API docs and lib" >> $ALLOY_NOTICE
+
 
 echo "# Creating the custom branch: $TMP_BRANCH"
 git checkout -q -b "$TMP_BRANCH" > /dev/null
